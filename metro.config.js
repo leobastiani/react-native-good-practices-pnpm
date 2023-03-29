8d10802c1d78240055b65997ab91e50b5368fbfc
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-03-29 00:17:49 -0300

    pnpm working

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,16 @@
+const {makeMetroConfig} = require('@rnx-kit/metro-config');
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
+
 /**
  * Metro configuration for React Native
  * https://github.com/facebook/react-native
  *
  * @format
  */
-
-module.exports = {
+module.exports = makeMetroConfig({
+  resolver: {
+    resolveRequest: MetroSymlinksResolver(),
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -14,4 +19,4 @@ module.exports = {
       },
     }),
   },
-};
+});
